e9c75f2d78bb1d097a5e64e451feed6b3fba4fdd
jgalbal <75653294+jgalbal@users.noreply.github.com>
2022-02-03 23:55:37 -0500

    Add files via upload

---
+++ josegalvez-configserver/src/test/java/com/josegalvez/configserver/JosegalvezConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josegalvez.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JosegalvezConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
